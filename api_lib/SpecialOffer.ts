b91e5cc19c10133519ac33c0e7e233e5bac9c75f
tvkspor <tmtuan120704@gmail.com>
2024-10-26 21:19:07 +0700

    Update api_lib

---
+++ api_lib/SpecialOffer.ts
@@ -0,0 +1,138 @@
+import axios from "axios";
+
+export interface SpecialOfferInfo {
+  id: number;
+  Description: string;
+  Type: string;
+  StartDate: string;
+  EndDate: string;
+  MinQty: number;
+  MaxQty: number;
+  DiscountPct: string;
+}
+
+export interface UpdateSpecialOffer {
+  specialOfferID: String;
+  Description: string;
+  Type: string;
+  StartDate: string;
+  EndDate: string;
+  MinQty: number;
+  MaxQty: number;
+  DiscountPct: string;
+}
+
+export interface CreateSpecialOffer {
+  Description: string;
+  DiscountPct: string;
+  Type: string;
+  StartDate: string;
+  EndDate: string;
+  MinQty: number;
+  MaxQty: number;
+}
+
+export interface DeleteSpecialOffer {
+  specialOfferID: String;
+}
+
+interface SpecialOfferPayload {
+  token: string;
+}
+
+export class SpecialOfferOperation {
+  private baseUrl: string;
+
+  constructor() {
+    this.baseUrl = `${process.env.NEXT_PUBLIC_BACKEND_LOGIN_ENDPOINT!}/sales`;
+  }
+
+  async getSpecialOffer(payload: SpecialOfferPayload) {
+    try {
+      const response = await axios.get(`${this.baseUrl}/getallspecialoffer/`, {
+        headers: {
+          Authorization: `Bearer ${payload.token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      return response.status === 200
+        ? { error: false, data: response.data as SpecialOfferInfo }
+        : { error: true, data: null };
+    } catch (err: any) {
+      return { error: true, data: null };
+    }
+  }
+
+  async updateSpecialOfferInfo(
+    payload: SpecialOfferPayload,
+    updateData: UpdateSpecialOffer
+  ) {
+    try {
+      const response = await axios.post(
+        `${this.baseUrl}/editspecialoffer/`,
+        updateData,
+        {
+          headers: {
+            Authorization: `Bearer ${payload.token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      return response.status === 202
+        ? { error: false, data: response.data as SpecialOfferInfo }
+        : { error: true, data: null };
+    } catch (err: any) {
+      return { error: true, data: null };
+    }
+  }
+
+  async deleteSpecialOfferInfo(
+    payload: SpecialOfferPayload,
+    updateData: DeleteSpecialOffer
+  ) {
+    try {
+      const response = await axios.post(
+        `${this.baseUrl}/deletespecialoffer/`,
+        updateData,
+        {
+          headers: {
+            Authorization: `Bearer ${payload.token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      return response.status === 202
+        ? { error: false, data: response.data as SpecialOfferInfo }
+        : { error: true, data: null };
+    } catch (err: any) {
+      return { error: true, data: null };
+    }
+  }
+
+  async createSpecialOfferInfo(
+    payload: SpecialOfferPayload,
+    updateData: CreateSpecialOffer
+  ) {
+    try {
+      const response = await axios.post(
+        `${this.baseUrl}/createspecialoffer/`,
+        updateData,
+        {
+          headers: {
+            Authorization: `Bearer ${payload.token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      return response.status === 202
+        ? { error: false, data: response.data as SpecialOfferInfo }
+        : { error: true, data: null };
+    } catch (err: any) {
+      return { error: true, data: null };
+    }
+  }
+}
