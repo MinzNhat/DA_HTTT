ca754158be225a202847c1bf7ea1646def6bf2d7
Trần Mạnh Tuấn <tmtuan120704@gmail.com>
2024-12-30 21:33:11 +0700

    Update UserFields.tsx

--- app/(dashboard)/user/components/UserFields.tsx
+++ app/(dashboard)/user/components/UserFields.tsx
@@ -1,25 +1,55 @@
 import InputField from "@/components/fields";
 import { FormattedMessage } from "react-intl";
-import { CustomerInfo, UpdateUserInfo } from "@/api_lib/User";
+import {
+  CustomerInfo,
+  CustomerStore,
+  CustomerIndividual,
+  UpdateUserInfo,
+} from "@/api_lib/User";
+
+type NestedCustomerInfoKey =
+  | keyof CustomerInfo
+  | `CustomerStore.${keyof CustomerStore}`
+  | `CustomerIndividual.${keyof CustomerIndividual}`;
 
 interface Props {
   data: CustomerInfo | UpdateUserInfo;
   fields: Array<{
-    id: keyof CustomerInfo | keyof UpdateUserInfo;
+    id: NestedCustomerInfoKey;
     type: string;
     label: string;
     disable?: boolean;
     important?: boolean;
-    onChange?: (
-      id: keyof CustomerInfo | keyof UpdateUserInfo,
-      value: string
-    ) => void;
+    onChange?: (id: NestedCustomerInfoKey, value: string) => void;
   }>;
-  handleChange: (
-    id: keyof CustomerInfo | keyof UpdateUserInfo,
-    value: string | number
-  ) => void;
+  handleChange: (id: NestedCustomerInfoKey, value: string | number) => void;
 }
+
+const getNestedFieldValue = (
+  data: CustomerInfo,
+  id: NestedCustomerInfoKey
+): string => {
+  const keys = id.split(".");
+  if (keys.length === 1) {
+    return data[keys[0] as keyof CustomerInfo]
+      ? data[keys[0] as keyof CustomerInfo]!.toString()
+      : "không có dữ liệu";
+  } else if (keys.length === 2) {
+    const [parentKey, childKey] = keys;
+    if (parentKey === "CustomerStore" && data.CustomerStore) {
+      return data.CustomerStore[childKey as keyof CustomerStore]
+        ? data.CustomerStore[childKey as keyof CustomerStore]!.toString()
+        : "không có dữ liệu";
+    } else if (parentKey === "CustomerIndividual" && data.CustomerIndividual) {
+      return data.CustomerIndividual[childKey as keyof CustomerIndividual]
+        ? data.CustomerIndividual[
+            childKey as keyof CustomerIndividual
+          ]!.toString()
+        : "không có dữ liệu";
+    }
+  }
+  return "không có dữ liệu";
+};
 
 const UserFields: React.FC<Props> = ({ data, fields, handleChange }) => {
   return (
@@ -42,7 +72,7 @@ const UserFields: React.FC<Props> = ({ data, fields, handleChange }) => {
               id={label}
               disabled={disable}
               type={type}
-              value={data[id as keyof typeof data] as string}
+              value={getNestedFieldValue(data as CustomerInfo, id)}
               setValue={(value: string) =>
                 onChange ? onChange(id, value) : handleChange(id, value)
               }
